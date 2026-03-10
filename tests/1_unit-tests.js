a206894906ad1a7d23fc4edeccd767b7270c7abe
“Julio <87451941+JYurai@users.noreply.github.com>
2026-03-09 21:28:27 -0500

    include test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -92,7 +92,7 @@ suite('Unit Tests', function () {
     });
     // #12
     test('Array #include, #notInclude', function () {
-      assert.include(winterMonths, 'jul', "It's summer in july...");
+      assert.notInclude(winterMonths, 'jul', "It's summer in july...");
       assert.include(backendLanguages, 'javascript', 'JS is a backend language');
     });
   });
