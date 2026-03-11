f54b8b914845e4d8fab0f5fcf1653cf216e6f503
“Julio <87451941+JYurai@users.noreply.github.com>
2026-03-10 20:16:35 -0500

    json test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -39,7 +39,10 @@ suite('Functional Tests', function () {
         .request(server)
         .keepOpen()
         .put('/travellers')
-
+        .send({ 
+          name: 'Cristoforo',
+          surname: 'Colombo'
+         })
         .end(function (err, res) {
           assert.fail();
 
