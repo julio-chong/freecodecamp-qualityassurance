49293d3a82480633941bd149d45c29673c65ff86
“Julio <87451941+JYurai@users.noreply.github.com>
2026-03-13 17:18:58 -0500

    colombo browser test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -106,10 +106,15 @@ suite('Functional Tests with Zombie.js', function () {
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
+    test('submit "surname" : "Colombo" - write your e2e test...', function(done) {
+      browser.fill('surname', 'Colombo').pressButton('submit', function() {
+        browser.assert.success();
+        browser.assert.text('span#name', 'Cristoforo');
+        browser.assert.text('span#surname', 'Colombo');
+        browser.assert.element('span#dates', 1);
 
-      done();
+        done();
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
