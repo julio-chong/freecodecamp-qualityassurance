c0d075d19eabce6432ffe9c4a48fecccdab65539
“Julio <87451941+JYurai@users.noreply.github.com>
2026-03-13 17:31:15 -0500

    colombo browser test

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -106,12 +106,18 @@ suite('Functional Tests with Zombie.js', function () {
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
-    test('submit "surname" : "Colombo" - write your e2e test...', function(done) {
-      browser.fill('surname', 'Colombo').pressButton('submit', function() {
+    test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
+      // fill the form, and submit.
+      browser.fill('surname', 'Vespucci').pressButton('submit', function() {
+        // assert that status is OK 200
         browser.assert.success();
-        browser.assert.text('span#name', 'Cristoforo');
-        browser.assert.text('span#surname', 'Colombo');
+        // assert that the text inside the element 'span#name' is 'Amerigo'
+        browser.assert.text('span#name', 'Amerigo');
+        // assert that the text inside the element 'span#surname' is 'Vespucci'
+        browser.assert.text('span#surname', 'Vespucci');
+        // assert that the element(s) 'span#dates' exist and their count is 1
         browser.assert.element('span#dates', 1);
+
         done();
       });
     });
